2854d9fb4eba9400063f8b3918a23d66a7ca8f0e
marchingon12 <augustthegreat.cool@gmail.com>
2020-12-13 10:04:25 +0100

    Android: revert repo for changelog

--- perry/modules/android.py
+++ perry/modules/android.py
@@ -24,7 +24,7 @@ def magisk(update, context):
         data = get(url + branch[0] + ".json").json()
         releases += (
             f"*{type}*: \n"
-            f"• [Changelog](https://github.com/SensiPeeps/magisk_files/blob/{branch[1]}/notes.md)\n"
+            f"• [Changelog](https://github.com/topjohnwu/magisk_files/blob/{branch[1]}/notes.md)\n"
             f'• Zip - [{data["magisk"]["version"]}-{data["magisk"]["versionCode"]}]({data["magisk"]["link"]}) \n'
             f'• App - [{data["app"]["version"]}-{data["app"]["versionCode"]}]({data["app"]["link"]}) \n'
             f'• Uninstaller - [{data["magisk"]["version"]}-{data["magisk"]["versionCode"]}]({data["uninstaller"]["link"]})\n\n'
